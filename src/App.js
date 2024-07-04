da0b254ba41ca268f8705b5bf0d2fcacea73753d
ChandithWickramasinghe <chandith2014@gmail.com>
2024-07-04 10:24:39 +0530

    Project Completed

---
+++ src/App.js
@@ -0,0 +1,243 @@
+import './App.css';
+
+function App() {
+  return (
+        
+    <div className="App1" class="container-fuid">
+       
+    <section>
+      
+      <sec1>
+
+        <img src="CDE_6385.jpg" className="DP" width="150" height="150"></img>
+        <br></br>
+
+        <p>
+          <div>
+            <div class="mnt">
+            <b>CONTACT</b>
+            </div>
+            <hr class="hrw" width="90%" align="left"></hr>
+            <p style={{ fontSize: '16px' }}>
+              <div class="details">
+              <img src="https://www.citypng.com/public/uploads/preview/-121610564853gg73ucwrdk.png" width="30" height="30">
+              </img>
+              &nbsp; +94 76 125 2841<br></br>
+              </div>
+              <div class="details">
+              <img src="https://th.bing.com/th/id/OIP.-VJgyMiLg4SEfa--HBpwwwHaHa?w=512&h=512&rs=1&pid=ImgDetMain" width="30" height="30">
+              </img>
+              &ensp; chandith2014@gmail.com<br></br>
+              </div>
+              <div class="details">
+              <img src="https://static.vecteezy.com/system/resources/previews/000/627/684/large_2x/vector-house-icon-symbol-sign.jpg" width="30" height="30">
+              </img>
+              &ensp; 163/2, Aluthwewa, A/pelessa<br></br>
+              </div>
+              &emsp; &emsp; Tangalle, Sri Lanka
+            </p>
+          </div>
+        </p><br></br>
+
+        <p>
+          <div>
+            <div class="mnt">
+            <b>TECHNICAL SKILLS</b>
+            </div>
+            <hr class="hrw" width="90%" align="left"></hr>
+            <p style={{ fontSize: '16px' }}>
+              <ul class="list-group">
+                <li>JAVA</li>
+                <li>PYTHON</li> 
+                <li>HTML</li>
+                <li>CSS</li> 
+                <li>C#</li>
+                <li>SQL</li> 
+                <li>REACT</li>
+              </ul>
+            </p>
+          </div>
+        </p><br></br>
+
+        <p>
+          <div>
+            <div class="mnt">
+            <b>PERSONAL SKILLS</b>
+            </div>
+            <hr class="hrw" width="90%" align="left"></hr>
+            <p style={{ fontSize: '16px' }}>
+              <ul class="list-group">
+                <li>Team Work</li>
+                <li>Time Management</li> 
+                <li>Leadership</li>
+                <li>Verbal and Written Communication</li>
+              </ul>
+            </p>
+          </div>
+        </p><br></br>
+
+        <p>
+          <div>
+            <div class="mnt">
+            <b>LANGUAGE</b>
+            </div>
+            <hr class="hrw" width="90%" align="left"></hr>
+            <p style={{ fontSize: '16px' }}>
+              <ul class="list-group">
+                <li>English</li>
+                <li>Sinhala</li>
+              </ul>
+            </p>
+          </div>
+        </p><br></br>
+
+        <p>
+          <div>
+            <div class="mnt">
+            <b>REFEREES</b>
+            </div>
+            <hr class="hrw" width="90%" align="left"></hr>
+            <p style={{ fontSize: '16px' }}>
+              <ul class="list-group">
+                <li>
+                  Mr. KAMAL D WERAHARA<br></br>
+                  Teacher<br></br>
+                  Phone: +94 71 3402121<br></br>
+                  Email: kamaldwerahara@gmail.com<br></br>
+                </li><br></br>
+                <li>
+                  Ms. LDTT DE SILVA<br></br>
+                  Lecturer - KDU<br></br>
+                  Phone: +94 76 9635871<br></br>
+                  Email: thumashids@kdu.ac.lk<br></br>
+                </li>
+              </ul>
+            </p>
+          </div>
+        </p><br></br>
+
+      </sec1>
+      
+      
+      <sec2>
+      
+        <h2 align="Left">CHANDITH <br></br> WICKRAMASINGHE</h2>
+        <div class="ungt">
+        &emsp; &emsp; &emsp; &emsp; &emsp; UNDERGRADUATE
+        </div>
+        
+        <p>
+          <div>
+            <div class="tpc">
+            <b>PROFILE</b>
+            </div>
+            <hr class="hr" width="100%" align="left"></hr>
+            <p style={{ fontSize: '16px' }}>
+            I aspire to leverage my skills and knowledge to make a meaningful impact in the field of IT. I'm with a keen interest to contribute those to reaal-world projects and need to learn the practical organizational knowledge.
+            </p>
+          </div>
+        </p>
+
+        <p>
+          <div>
+            <div class="tpc">
+            <b>EDUCATION</b>
+            </div>
+            <hr class="hr" width="100%" align="left"></hr>
+            <p style={{ fontSize: '18px' }}>
+            <b>UNDERGRADUATE</b><br />
+            <b>2021 - PRESENT</b>
+            </p>
+            <p style={{ fontSize: '16px' }}>
+            GENERAL SIR JOHN KOTELAWALA DEFENCE UNIVERSITY<br />
+            BSc.Hons Information Technology
+            </p>
+
+            <p style={{ fontSize: '18px' }}>
+            <b>SCHOOL STUDENT</b><br />
+            <b>January 2009 - August 2018</b>
+            </p>
+            <p style={{ fontSize: '16px' }}>
+            RUHUNU VIJAYABA COLLEGE, BELIATTA<br />
+            GCE Advanced Level - 2017,2018 (Biological Science)<br />
+            GCE Ordinary Level - 2014
+            </p>
+          </div>
+        </p>
+          
+        <p>
+          <div>
+            <div class="tpc">
+            <b>PUBLICATION</b>
+            </div>
+            <hr class="hr" width="100%" align="left"></hr>
+            <p style={{ fontSize: '18px' }}>
+            <b>A review paper</b>
+            </p>
+            <p style={{ fontSize: '16px' }}>
+            A review paper on Cloud computing security issues: Challenges and oppotunities for the 4th students' symposium of Faculty of computing KDU
+            </p>
+          </div>
+        </p>
+
+        <p>
+          <div>
+            <div class="tpc">
+            <b>UNIVERSITY PROJECTS</b>
+            </div>
+            <hr class="hr" width="100%" align="left"></hr>
+            <p style={{ fontSize: '18px' }}>
+            <b>VEHICLE SERVICE CENTER SYSTEM</b>
+            </p>
+            <p style={{ fontSize: '16px' }}>
+            Web application for vehicle service center for the Rapid application development module
+            </p>
+
+            <p style={{ fontSize: '18px' }}>
+            <b>LEAVE MANAGEMENT SYSTEM FOR KDU SOUTHERN CAMPUS</b>
+            </p>
+            <p style={{ fontSize: '16px' }}>
+            System for students leave management as the second-year project (Group)
+            </p>
+          </div>
+        </p>
+
+        <p>
+          <div>
+            <div class="tpc">
+            <b>CERTIFICATES</b>
+            </div>
+            <hr class="hr" width="100%" align="left"></hr>
+            <p style={{ fontSize: '18px' }}>
+            <b>Completed -</b>
+            </p>
+            <p style={{ fontSize: '16px' }}>
+            Programming in Python - 1. Python for Beginners by University of Moratuwa<br />
+            CCNAv7: Introduction to Networks by Cisco Networking Academy<br />
+            IT Essentials: PC Hardware and Software by Cisco Networking Academy<br />
+            CCNAv7: Switching, Routing, and Wireless Essentials by Cisco Networking Academy<br />
+            IoT Fundamentals: Connecting Things by Cisco Networking Academy<br />
+            IoT Fundamentals: IoT Security by Cisco Networking Academy<br />
+            IoT Fundamentals: Big Data & Analytics by Cisco Networking Academy
+            </p>
+
+            <p style={{ fontSize: '18px' }}>
+            <b>In Progress -</b>
+            </p>
+            <p style={{ fontSize: '16px' }}>
+            Trainee- Full Stack Developer by University of Moratuwa<br />
+            Web Development - 1. Web Design for Beginners by University of Moratuwa<br />
+            Project Management Skills by University of Moratuwa
+            </p>
+          </div>
+        </p>
+        
+      </sec2>
+
+    </section>
+      
+    </div>
+  );
+}
+
+export default App;
